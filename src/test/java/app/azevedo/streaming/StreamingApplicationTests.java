0fa22fc577a5856870c51b37cd91ff0f8655d155
Fernando Delgado Azevedo <fernandodelgadoazevedo@gmail.com>
2023-04-11 10:58:38 -0300

    ProjetoStreaming

---
+++ src/test/java/app/azevedo/streaming/StreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package app.azevedo.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
